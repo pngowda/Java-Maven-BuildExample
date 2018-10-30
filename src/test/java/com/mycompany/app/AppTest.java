34a2eba015d4d7a48788d4b5dbb8d245b7cbd009
Prajwal Gowda <pgowda@itemis.de>
2018-10-30 15:50:15 +0100

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
     @Test
     public void testAppConstructor() {
         try {
-            new App("test");
+            new App();
         } catch (Exception e) {
             fail("Construction failed.");
         }
