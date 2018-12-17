d1aecb76e14ef899f13f464a3a054212fda50d83
Prajwal Gowda <pgowda@itemis.de>
2018-12-17 13:23:52 +0100

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,13 +7,13 @@ public class App
 {
 
     private final String message1 = "Hello World!";
-    //private final String message2 = "Hello World!";
+    private final String message2 = "Hello World!";
 
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage1());
-        //System.out.println(new App().getMessage2());
+        System.out.println(new App().getMessage2());
     }
 
     private final String getMessage1() {
@@ -21,7 +21,7 @@ public class App
     }
     
    private final String getMessage2() {
-        return message1;
+        return message2;
     }
 
 }
